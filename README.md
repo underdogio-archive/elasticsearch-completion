9c2fca0ba9ac01f92e2ea644b9e4b7348e316235
Todd Wolfson <todd@twolfson.com>
2015-10-02 18:04:52 -0500

    Initial release

--- README.md
+++ README.md
@@ -2,19 +2,44 @@
 
 Low level query completion for Elasticsearch
 
+This was built as part of an internal tool to autocomplete queries for Elasticsearch.
+
 ## Getting Started
 Install the module with: `npm install elasticsearch-completion`
 
 ```js
-var elasticsearchCompletion = require('elasticsearch-completion');
-elasticsearchCompletion(); // 'awesome'
+// Create a completion class
+var ElasticsearchCompletion = require('elasticsearch-completion');
+var esCompletion = new ElasticsearchCompletion([
+    'name',
+    'location'
+]);
+
+// Supply some queries to complete against
+esCompletion.match(''); // ['name:', 'location:', '_exists_:', '_missing_:']
+esCompletion.match('na'); // ['name:']
+esCompletion.match('name:'); // []
+esCompletion.match('_exists_:'); // ['_exists_:name', '_exists_:location']
 ```
 
 ## Documentation
-_(Coming soon)_
+`elasticsearch-completion` exposes `ElasticsearchCompletion` via its `module.exports`.
 
-## Examples
-_(Coming soon)_
+### `new ElasticsearchCompletion(fields)`
+Constructor for a new completion class
+
+- fields `Array` - Names of fields to match against (e.g. `name`, `location`)
+    - For example, `['foo', 'bar']` would supply matches agains the fields `foo` and `bar`
+
+#### `esCompletion.match(query)`
+Find matching completions for an Elasticsearch query
+
+- query `String` - Query being used in Elasticsearch
+
+**Returns:**
+
+- matches `Array` - Collection of matching completions for the query
+    - For example, `na` might complete to `['name:']` and `_ex` might complete to `['_exists_:']`
 
 ## Contributing
 In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint via `npm run lint` and test via `npm test`.
--- test/elasticsearch-completion-test.js
+++ test/elasticsearch-completion-test.js
@@ -1,10 +1,191 @@
 // Load in dependencies
 var assert = require('assert');
-var elasticsearchCompletion = require('../');
+var parse = require('lucene-query-parser').parse;
+var ElasticsearchCompletion = require('../');
 
 // Start our tests
-describe('elasticsearch-completion', function () {
-  it('returns awesome', function () {
-    assert.strictEqual(elasticsearchCompletion(), 'awesome');
+describe('An instance of ElasticsearchCompletion', function () {
+  before(function createCompletion () {
+    this.esCompletion = new ElasticsearchCompletion([
+      'name',
+      'location'
+    ]);
+  });
+
+  describe('completing an empty string', function () {
+    it('returns all possible field completions including special ones', function () {
+      var matches = this.esCompletion.match('');
+      assert.deepEqual(matches, [
+        'name:',
+        'location:',
+        '_exists_:',
+        '_missing_:'
+      ]);
+    });
+  });
+
+  describe('completing a query with a partial field', function () {
+    describe('for a special field (e.g. `_exists_`, `_missing_`)', function () {
+      it('returns the query with matching field completions', function () {
+        var matches = this.esCompletion.match('_ex');
+        assert.deepEqual(matches, [
+          '_exists_:'
+        ]);
+      });
+    });
+
+    describe('for a normal field', function () {
+      it('returns the query with matching field completions', function () {
+        var matches = this.esCompletion.match('na');
+        assert.deepEqual(matches, [
+          'name:'
+        ]);
+      });
+    });
+
+    describe('for no matching fields', function () {
+      it('returns no matches', function () {
+        var matches = this.esCompletion.match('notafield');
+        assert.deepEqual(matches, []);
+      });
+    });
+  });
+
+  describe('completing a query with a field and a colon', function () {
+    describe('for a special field (e.g. `_exists_`, `_missing_`)', function () {
+      it('returns the query with matching term completions', function () {
+        var matches = this.esCompletion.match('_missing_:');
+        assert.deepEqual(matches, [
+          '_missing_:name',
+          '_missing_:location'
+        ]);
+      });
+    });
+
+    describe('for a normal field', function () {
+      // TODO: This should become matching hints
+      it('returns no matches', function () {
+        var matches = this.esCompletion.match('name:');
+        assert.deepEqual(matches, []);
+      });
+    });
+
+    describe('for no matching fields', function () {
+      it('returns no matches', function () {
+        var matches = this.esCompletion.match('doesnotexist:');
+        assert.deepEqual(matches, []);
+      });
+    });
+  });
+
+  describe('completing a query with a field, colon, and partial term', function () {
+    describe('for a special field (e.g. `_exists_`, `_missing_`)', function () {
+      it('returns the query with matching fields as term completions', function () {
+        var matches = this.esCompletion.match('_exists_:n');
+        assert.deepEqual(matches, [
+          '_exists_:name'
+        ]);
+      });
+    });
+
+    describe('for a normal field', function () {
+      // TODO: This should become matching hints
+      it('returns no matches', function () {
+        var matches = this.esCompletion.match('name:to');
+        assert.deepEqual(matches, []);
+      });
+    });
+
+    describe('for no matching fields', function () {
+      it('returns no matches', function () {
+        var matches = this.esCompletion.match('unknown:wa');
+        assert.deepEqual(matches, []);
+      });
+    });
+  });
+
+  describe('completing a query with trailing whitespace', function () {
+    it('returns all possible field completions including special ones', function () {
+      var matches = this.esCompletion.match('_exists_:name ');
+      assert.deepEqual(matches, [
+        '_exists_:name name:',
+        '_exists_:name location:',
+        '_exists_:name _exists_:',
+        '_exists_:name _missing_:'
+      ]);
+    });
+  });
+});
+
+// Inner unit tests
+describe('A query with 1 explicit ANDs', function () {
+  describe('when parsed via ElasticsearchCompletion.getRightmostResult', function () {
+    it('returns the rightmost result', function () {
+      var results = parse('foo:bar AND fuu:baz');
+      assert.deepEqual(ElasticsearchCompletion._getRightmostResult(results), {
+        field: 'fuu',
+        term: 'baz'
+      });
+    });
+  });
+});
+
+describe('A query with 2 explicit ANDs', function () {
+  describe('when parsed via ElasticsearchCompletion.getRightmostResult', function () {
+    it('returns the rightmost result', function () {
+      var results = parse('foo:bar AND fuu:baz AND aaa:bbb');
+      assert.deepEqual(ElasticsearchCompletion._getRightmostResult(results), {
+        field: 'aaa',
+        term: 'bbb'
+      });
+    });
+  });
+});
+
+describe('A query with 1 implicit ANDs', function () {
+  describe('when parsed via ElasticsearchCompletion.getRightmostResult', function () {
+    it('returns the rightmost result', function () {
+      var results = parse('foo:bar fuu:baz');
+      assert.deepEqual(ElasticsearchCompletion._getRightmostResult(results), {
+        field: 'fuu',
+        term: 'baz'
+      });
+    });
+  });
+});
+
+describe('A query with 2 implicit ANDs', function () {
+  describe('when parsed via ElasticsearchCompletion.getRightmostResult', function () {
+    it('returns the rightmost result', function () {
+      var results = parse('foo:bar fuu:baz aaa:bbb');
+      assert.deepEqual(ElasticsearchCompletion._getRightmostResult(results), {
+        field: 'aaa',
+        term: 'bbb'
+      });
+    });
+  });
+});
+
+describe('A query with 2 fields with no terms', function () {
+  describe('when parsed via ElasticsearchCompletion.getRightmostResult', function () {
+    it('returns the rightmost result', function () {
+      var results = parse('foo AND fuu');
+      assert.deepEqual(ElasticsearchCompletion._getRightmostResult(results), {
+        field: ElasticsearchCompletion.IMPLICIT,
+        term: 'fuu'
+      });
+    });
+  });
+});
+
+describe('A query with 3 fields with no terms', function () {
+  describe('when parsed via ElasticsearchCompletion.getRightmostResult', function () {
+    it('returns the rightmost result', function () {
+      var results = parse('foo AND fuu AND aaa');
+      assert.deepEqual(ElasticsearchCompletion._getRightmostResult(results), {
+        field: ElasticsearchCompletion.IMPLICIT,
+        term: 'aaa'
+      });
+    });
   });
 });
